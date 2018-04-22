081a78da993c5290e4fa1bb0b8012b9c46ee2f47
ariesjh <aries@LAPTOP-jhjj>
2018-04-22 10:59:35 -0700

    FIrst Commit

---
+++ AHPortfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AHPortfolio.MvcApplication" Language="C#" %>
---
+++ AHPortfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AHPortfolio.Startup))]
+namespace AHPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
